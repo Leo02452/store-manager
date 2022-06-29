225075b0e92a7f5d03de66987adb7f4c8c95930d
Leo Araujo <leonardo_02452@hotmail.com>
2022-06-29 16:31:15 -0300

    feat: implementada nova rota e novo middleware
    A rota de produtos e o middleware de erro foram implementos no projeto.
    
    Requisito 1 do projeto concluído.

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+require('express-async-errors');
+const productsRouter = require('./routes/productsRouter');
 
 const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.use('/products', productsRouter);
+
+app.use((err, _req, res, _next) => {
+  switch (err.name) {
+    case 'NotFoundError':
+      res.status(404).json({ message: err.message });
+      break;
+    default:
+      res.status(500).json({ message: err.message });
+  }
 });
 
 // não remova essa exportação, é para o avaliador funcionar
