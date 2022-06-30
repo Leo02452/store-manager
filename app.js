7015cad3021fa9d117ae4c0898a6e8d7af136574
Leo Araujo <leonardo_02452@hotmail.com>
2022-06-30 16:46:00 -0300

    feat: adicionado parseamento JSON do body

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('express-async-errors');
 const productsRouter = require('./routes/productsRouter');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
