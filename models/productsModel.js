a86a7a45bdb505ae77b3748328b5e13378dbe9b2
Leo Araujo <leonardo_02452@hotmail.com>
2022-06-30 18:07:29 -0300

    fix: mudando o nome do banco de dados

--- models/productsModel.js
+++ models/productsModel.js
@@ -2,12 +2,12 @@ const db = require('./db');
 
 const productsModel = {
   async list() {
-    const query = 'SELECT * FROM db.products';
+    const query = 'SELECT * FROM StoreManager.products';
     const [products] = await db.query(query);
     return products;
   },
   async getById(id) {
-    const query = 'SELECT * FROM db.products WHERE id = ?';
+    const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
     const [[product]] = await db.query(query, [id]);
     return product;
   },
