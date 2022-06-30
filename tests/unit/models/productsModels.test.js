6edbea33b3939fe10cb84bd4401ef4c2c93060e4
Leo Araujo <leonardo_02452@hotmail.com>
2022-06-30 16:48:43 -0300

    test: para 'produtos' da camada model
    Criado novo teste para as funções list e getById de produtos.

---
+++ tests/unit/models/productsModels.test.js
@@ -0,0 +1,57 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const productsModel = require('../../../models/productsModel');
+const db = require('../../../models/db');
+
+beforeEach(() => {
+  sinon.restore();
+});
+
+describe('models/productsModel', () => {
+  describe('list', () => {
+    it('should return a list of products', async () => {
+      const result = [
+        {
+          "id": 1,
+          "name": "Martelo de Thor"
+        },
+        {
+          "id": 2,
+          "name": "Traje de encolhimento"
+        },
+        {
+          "id": 3,
+          "name": "Escudo do Capitão América"
+        }
+      ];
+
+      sinon.stub(db, 'query').resolves([result]);
+      const productsList = await productsModel.list();
+      expect(productsList).to.be.a('array');
+      expect(productsList.length).to.be.equal(3);
+    });
+  });
+
+  describe('getById', () => {
+    it('should return a object', async () => {
+      const result = { "id": 1, "name": "Martelo de Thor" };
+      sinon.stub(db, 'query').resolves([[result]]);
+
+      const product = await productsModel.getById(1);
+      expect(product).to.be.a('object');
+    });
+    it('should object have a property id', async () => {
+      const result = { "id": 1, "name": "Martelo de Thor" };
+      sinon.stub(db, 'query').resolves([[result]]);
+      const product = await productsModel.getById(1);
+      expect(product).to.have.a.property('id');
+    });
+    it('should object have a property name', async () => {
+      const result = { "id": 1, "name": "Martelo de Thor" };
+      sinon.stub(db, 'query').resolves([[result]]);
+      const product = await productsModel.getById(1);
+      expect(product).to.have.a.property('name');
+      expect(product.name).to.be.equal('Martelo de Thor');
+    });
+  });
+});
