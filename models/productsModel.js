1a15b5ca44d7dc351e28db1bab68dab06b7d5ae3
Leo Araujo <leonardo_02452@hotmail.com>
2022-06-30 17:49:15 -0300

    fix: mudando query para o banco de dados
    Adicionado o nome do banco antes da tabela na hora que fazer a query

--- models/productsModel.js
+++ models/productsModel.js
@@ -2,12 +2,12 @@ const db = require('./db');
 
 const productsModel = {
   async list() {
-    const query = 'SELECT * FROM products';
+    const query = 'SELECT * FROM db.products';
     const [products] = await db.query(query);
     return products;
   },
   async getById(id) {
-    const query = 'SELECT * FROM products WHERE id = ?';
+    const query = 'SELECT * FROM db.products WHERE id = ?';
     const [[product]] = await db.query(query, [id]);
     return product;
   },
