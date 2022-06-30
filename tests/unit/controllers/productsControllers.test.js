5edf9d4c8eef3ba6bf17dd3f72d7ca5073742ab2
Leo Araujo <leonardo_02452@hotmail.com>
2022-06-30 16:49:45 -0300

    test: para 'produtos' da camada controller
    Criado novo teste para as funções list e getById de produtos.
    
    Requisitos 2, 5, 7 e 9 concluídos.

---
+++ tests/unit/controllers/productsControllers.test.js
@@ -0,0 +1,75 @@
+const { expect, use } = require('chai');
+const sinon = require('sinon');
+const chaiAsPromised = require('chai-as-promised');
+const productsService = require('../../../services/productsService');
+const productsController = require('../../../controllers/productsController');
+const NotFoundError = require('../../../services/errors');
+
+use(chaiAsPromised);
+
+beforeEach(() => {
+  sinon.restore();
+});
+
+describe('controllers/productsController', () => {
+  describe('list should return status code', () => {
+    it('200 and a list of products', async () => {
+      const req = {};
+      const res = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub();
+
+      const result = [
+        {
+          "id": 1,
+          "name": "Martelo de Thor"
+        },
+        {
+          "id": 2,
+          "name": "Traje de encolhimento"
+        },
+        {
+          "id": 3,
+          "name": "Escudo do Capitão América"
+        }
+      ];
+
+      sinon.stub(productsService, 'list').resolves(result);
+      await productsController.list(req, res);
+
+      expect(res.status.calledWith(200)).to.be.equal(true);
+      expect(res.json.calledWith(result)).to.be.equal(true);
+    });
+  });
+  describe('getById', () => {
+    it('200 when id is valid', async () => {
+      const req = {};
+      const res = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub();
+
+      req.params = { id: 1 };
+
+      sinon.stub(productsService, 'getById').resolves({ "id": 1, "name": "Martelo de Thor" });
+      await productsController.getById(req, res);
+
+      expect(res.status.calledWith(200)).to.be.equal(true);
+      expect(res.json.calledWith({ "id": 1, "name": "Martelo de Thor" })).to.be.equal(true);
+    });
+    it('404 when id is invalid', async () => {
+      const req = {};
+      const res = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub();
+
+      req.params = { id: 'claudio' };
+
+      sinon.stub(productsService, 'getById').resolves(undefined);
+
+      expect(productsController.getById(req, res)).to.be.rejectedWith(NotFoundError);
+    });
+  });
+});
