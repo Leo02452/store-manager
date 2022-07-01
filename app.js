e9059109bd913efad0acc80d61041a77d2922e03
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-01 18:26:12 -0300

    feat: add o tipo validação no middleware de erro

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use((err, _req, res, _next) => {
     case 'NotFoundError':
       res.status(404).json({ message: err.message });
       break;
+    case 'ValidationError':
+      res.status(err.code).json({ message: err.message });
+      break;
     default:
       res.status(500).json({ message: err.message });
   }
