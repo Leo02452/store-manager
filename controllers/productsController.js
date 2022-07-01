a9a5bd6e48880459b947defaa3a8c9193b1891da
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-01 20:29:08 -0300

    feat: validação da requisição para editar produto

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -16,6 +16,12 @@ const productsController = {
     const product = await productsService.add(name);
     res.status(201).json(product);
   },
+  async update(req, res) {
+    const { id } = req.params;
+    const { name } = await productsService.validateBodyAdd(req.body);
+    const product = await productsService.update(id, name);
+    res.status(200).json(product);
+  },
 };
 
 module.exports = productsController;
