1c6c960066f3ce4b6c134faf63609b6471b32d43
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-06 11:18:33 -0300

    feat: implementadas validações pra add vendas
    Na camada de controller foi criada função que
    valida requisição e regras de negocio antes de cadastrar venda

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,4 +1,5 @@
 const salesService = require('../services/salesService');
+const productsService = require('../services/productsService');
 
 const salesController = {
   async list(_req, res) {
@@ -10,6 +11,19 @@ const salesController = {
     const { id } = req.params;
     const product = await salesService.getById(id);
     res.status(200).json(product);
+  },
+  async add(req, res) {
+    const sales = req.body;
+
+    const bodyPromises = sales.map((sale) => salesService.validateBodyAdd(sale));
+    const validatedSales = await Promise.all(bodyPromises);
+
+    const promises = validatedSales
+      .map(({ productId }) => productsService.checkIfExists(productId));
+    await Promise.all(promises);
+
+    const saleInfo = await salesService.add(sales);
+    res.status(201).json(saleInfo);
   },
 };
 
