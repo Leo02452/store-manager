adfa9226014b70b2efab5e0e03ffba0001158596
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-07 00:04:39 -0300

    refactor: movida validação de id para controller
    A função que valida se um produto existe agora é chamada no controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,33 +3,47 @@ const productsService = require('../services/productsService');
 const productsController = {
   async list(_req, res) {
     const productsList = await productsService.list();
+
     res.status(200).json(productsList);
   },
-
   async getById(req, res) {
     const { id } = req.params;
+
+    await productsService.checkIfExists(id);
     const product = await productsService.getById(id);
+
     res.status(200).json(product);
   },
   async getByName(req, res) {
     const { q } = req.query;
+
     const products = await productsService.getByName(q);
+
     res.status(200).json(products);
   },
   async add(req, res) {
-    const { name } = await productsService.validateBodyAdd(req.body);
+    const { name } = await productsService.validateBody(req.body);
+
     const product = await productsService.add(name);
+
     res.status(201).json(product);
   },
   async update(req, res) {
+    const { name } = await productsService.validateBody(req.body);
     const { id } = req.params;
-    const { name } = await productsService.validateBodyAdd(req.body);
+
+    await productsService.checkIfExists(id);
+
     const product = await productsService.update(id, name);
+
     res.status(200).json(product);
   },
   async remove(req, res) {
     const { id } = req.params;
+
+    await productsService.checkIfExists(id);
     await productsService.remove(id);
+
     res.status(204).end();
   },
 };
