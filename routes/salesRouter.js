1cccd16ce437828d5f22e0c60bdc2dda4e3447af
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-07 11:11:11 -0300

    refactor: padronização da ordem de rotas

--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -7,10 +7,10 @@ salesRouter.get('/:id', salesController.getById);
 
 salesRouter.get('/', salesController.list);
 
+salesRouter.put('/:id', salesController.update);
+
 salesRouter.post('/', salesController.add);
 
 salesRouter.delete('/:id', salesController.remove);
-
-salesRouter.put('/:id', salesController.update);
 
 module.exports = salesRouter;
