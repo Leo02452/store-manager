1d02f09e2c1eb2f17f9613db36229afe7e62668a
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-09 13:46:09 -0300

    test: funções de produto na camada de services
    Funções refatoradas em menos linhas
    Foram adicionados casos de erro em todas as funções, menos validateBody
    Foram adicionados testes para funções de:
    validateBody, checkIfExists, getByName e remove

--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -4,17 +4,43 @@ const chaiAsPromised = require('chai-as-promised');
 const productsModel = require('../../../models/productsModel');
 const productsService = require('../../../services/productsService');
 const NotFoundError = require('../../../services/notFoundError');
-const { ValidationError } = require('joi');
+// const { ValidationError } = require('joi');
 
 use(chaiAsPromised);
 
-beforeEach(() => {
-  sinon.restore();
-});
-
 describe('services/productsService', () => {
+  beforeEach(sinon.restore);
+
+  describe('validateBody', () => {
+    it('should return the body when the body is valid', () => {
+      const validBody = { name: "Hulkbuster" };
+
+      expect(productsService.validateBody(validBody)).to.be.deep.equal(validBody);
+    });
+    // it('should thrown an error if the body is empty', () => {
+    //   return expect(productsService.validateBody({})).to.eventually.be.rejectedWith(ValidationError);
+    // });
+  });
+
+  describe('checkIfExists', () => {
+    it('should return nothing in successfully case', () => {
+      sinon.stub(productsModel, 'getById').resolves({ "id": 1, "name": "Martelo de Thor" });
+
+      return expect(productsService.checkIfExists(1)).to.eventually.be.undefined;
+    });
+    it('should thrown an error if product is not found', () => {
+      sinon.stub(productsModel, 'getById').resolves(undefined);
+
+      return expect(productsService.checkIfExists(0)).to.eventually.be.rejectedWith(NotFoundError);
+    });
+    it('should be rejected when seriesModel.getById is rejected', () => {
+      sinon.stub(productsModel, 'getById').rejects();
+      return expect(productsService.checkIfExists(1)).to.eventually.be.rejected;
+    });
+  });
+
   describe('list', () => {
-    it('should return a list of products', async () => {
+    it('should return a list of products when productsModel.list returns a list of products', () => {
       const result = [
         {
           "id": 1,
@@ -31,53 +57,75 @@ describe('services/productsService', () => {
       ];
 
       sinon.stub(productsModel, 'list').resolves(result);
-      const productsList = await productsService.list();
-      expect(productsList).to.be.a('array');
-      expect(productsList.length).to.be.equal(3);
+
+      return expect(productsService.list()).to.eventually.deep.equal(result);
+    });
+    it('should be rejected when productsModel.list is rejected', () => {
+      sinon.stub(productsModel, 'list').rejects();
+      return expect(productsService.list()).to.eventually.be.rejected;
     });
   });
+
   describe('getById', () => {
-    it('should return a object', async () => {
+    it('should return an object when productsModel.getById returns an object', () => {
       const result = { "id": 1, "name": "Martelo de Thor" };
       sinon.stub(productsModel, 'getById').resolves(result);
 
-      const product = await productsService.getById(1);
-      expect(product).to.be.a('object');
+      return expect(productsService.getById(1)).to.eventually.deep.equal(result);
     });
-    it('should throw an error when productId is not found', async () => {
-      sinon.stub(productsModel, 'getById').resolves(undefined);
-
-      return expect(productsService.getById(0)).to.be.rejectedWith(NotFoundError);
+    it('should be rejected when productsModel.getById is rejected', () => {
+      sinon.stub(productsModel, 'getById').rejects();
+      return expect(productsService.getById(1)).to.eventually.be.rejected;
     });
   });
+
+  describe('getByName', () => {
+    it('should return a filtered list when productsModel.getByName returns a filtered list', () => {
+      const result = [{ "id": 1, "name": "Martelo de Thor" }];
+
+      sinon.stub(productsModel, 'getByName').resolves(result);
+
+      return expect(productsService.getByName("Martelo")).to.eventually.deep.equal(result);
+    });
+    it('should be rejected when productsModel.getByName is rejected', () => {
+      sinon.stub(productsModel, 'getByName').rejects();
+      return expect(productsService.getByName("Martelo")).to.eventually.be.rejected;
+    });
+  });
+
   describe('add', () => {
-    it('should return a object', async () => {
-      const insertId = 4;
-      sinon.stub(productsModel, 'add').resolves(insertId);
+    it('should return an object when productsModel.add returns an id', () => {
+      sinon.stub(productsModel, 'add').resolves(4);
 
-      const result = { "id": 4, "name": "Cinto do Batman" };
-      sinon.stub(productsModel, 'getById').resolves(result);
-
-      const product = await productsService.add({ "name": "Cinto do Batman" });
-      expect(product).to.be.a('object');
+      return expect(productsService.add("Manopla do Thanos")).to.eventually.deep.equal({ id: 4, name: "Manopla do Thanos" });
     });
-    // it('should throw error when name is less than 5 characters', () => {
-    //   return expect(productsService.validateBodyAdd({ name: "Céu" })).to.be.rejectedWith("name length must be at least 5 characters long");
-    // });
+    it('should be rejected when productsModel.add is rejected', () => {
+      sinon.stub(productsModel, 'add').rejects();
+      return expect(productsService.add("Manopla do Thanos")).to.eventually.be.rejected;
+    });
   });
+
   describe('update', () => {
-    it('should return a object', async () => {
-      sinon.stub(productsModel, 'getById').resolves({ "id": 1, "name": "Martelo de Thor" });
-      const affectedRows = 1;
-      sinon.stub(productsModel, 'update').resolves(affectedRows);
+    it('should return an object in successfully case', () => {
+      sinon.stub(productsModel, 'update').resolves();
 
-      const product = await productsService.update({ "id": 1, "name": "Traje do Pantera" });
-      expect(product).to.be.a('object');
+      return expect(productsService.update(1, "Traje do Pantera")).to.eventually.deep.equal({ id: 1, name: "Traje do Pantera" });
     });
-    it('should throw an error when productId is not found', async () => {
-      sinon.stub(productsModel, 'getById').resolves(undefined);
+    it('should be rejected when productsModel.update is rejected', () => {
+      sinon.stub(productsModel, 'update').rejects();
+      return expect(productsService.update(1, "Traje do Pantera")).to.eventually.be.rejected;
+    });
+  });
 
-      return expect(productsService.update(0)).to.be.rejectedWith(NotFoundError);
+  describe('remove', () => {
+    it('should return nothing in successfully case', () => {
+      sinon.stub(productsModel, 'remove').resolves();
+
+      return expect(productsService.remove(1)).to.eventually.be.undefined;
+    });
+    it('should be rejected when productsModel.remove is rejected', () => {
+      sinon.stub(productsModel, 'remove').rejects();
+      return expect(productsService.remove(1)).to.eventually.be.rejected;
     });
   });
 });
